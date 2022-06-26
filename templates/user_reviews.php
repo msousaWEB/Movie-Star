d21a56d1f6eb553382cc669147c9bddec4a81184
msousaWEB <msousa.dev@outlook.com>
2022-06-26 18:17:24 -0300

    User reviews.

--- templates/user_reviews.php
+++ templates/user_reviews.php
@@ -4,12 +4,12 @@
         <div class="col-md-1">
             <div class="profile-image-container review-image" style="background-image: url('<?=$app?>img/users/user.png')"></div>
             <div class="col-md-9 author-details-container">
-                <h4 class="author-name"><a href="#">User testando</a></h4>
-                <p><i class="fas fa-star"></i> 9</p>
+                <h4 class="author-name"><a href="#">Matheus</a></h4>
+                <p><i class="fas fa-star"></i><?=$review->rating?></p>
             </div>
             <div class="col-md-12">
                 <p class="coment-title">Comentário:</p>
-                <p>texto de comentarios</p>
+                <p><?=$review->review?></p>
             </div>
         </div>
     </div>
