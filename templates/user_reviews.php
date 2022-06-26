69bc0af5090afd821436c10d46cb8b6689046473
msousaWEB <msousa.dev@outlook.com>
2022-06-26 18:41:02 -0300

    hotfix;

--- templates/user_reviews.php
+++ templates/user_reviews.php
@@ -1,16 +1,14 @@
 <!--Comentários-->
 <div class="col-md-12 review">
     <div class="row">
-        <div class="col-md-1">
             <div class="profile-image-container review-image" style="background-image: url('<?=$app?>img/users/user.png')"></div>
-            <div class="col-md-9 author-details-container">
+            <div class="col author-details-container">
                 <h4 class="author-name"><a href="#">Matheus</a></h4>
                 <p><i class="fas fa-star"></i><?=$review->rating?></p>
             </div>
-            <div class="col-md-12">
+            <div class="col-md-9" style="padding-top: 20px;">
                 <p class="coment-title">Comentário:</p>
                 <p><?=$review->review?></p>
             </div>
-        </div>
     </div>
 </div>
